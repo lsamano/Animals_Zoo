f25dd5adf2568891da77a9944de7064305dc556a
lsamano <leizl.samano@gmail.com>
2018-12-13 14:45:40 -0500

    Done.

--- run.rb
+++ run.rb
@@ -3,18 +3,17 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-
-bronx_zoo = Zoo.new("Bronx Zoo", "Bronx")
-si_zoo = Zoo.new("Staten Zoo", "Staten Island")
-prospect_zoo = Zoo.new("Prospect Park Zoo", "Brooklyn")
-Zoo.find_by_location("Bronx")
-
-kat = Animal.new("cat","KittyKat",12, si_zoo)
-kitty = Animal.new("cat","Claud",10, bronx_zoo)
-puppy = Animal.new("dog","Puppup",20, prospect_zoo)
-dan = Animal.new("dog","Dan",30, prospect_zoo)
-pal = Animal.new("dog","Pal",25, prospect_zoo)
-sasha = Animal.new("cat","Sasha",20, prospect_zoo)
+# bronx_zoo = Zoo.new("Bronx Zoo", "Bronx")
+# si_zoo = Zoo.new("Staten Zoo", "Staten Island")
+# prospect_zoo = Zoo.new("Prospect Park Zoo", "Brooklyn")
+# Zoo.find_by_location("Bronx")
+#
+# kat = Animal.new("cat","KittyKat",12, si_zoo)
+# kitty = Animal.new("cat","Claud",10, bronx_zoo)
+# puppy = Animal.new("dog","Puppup",20, prospect_zoo)
+# dan = Animal.new("dog","Dan",30, prospect_zoo)
+# pal = Animal.new("dog","Pal",25, prospect_zoo)
+# sasha = Animal.new("cat","Sasha",20, prospect_zoo)
 
 binding.pry
 puts "done"
